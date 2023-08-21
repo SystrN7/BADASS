7a9971cf3a1f4fa73010d3d83378dee27a0d9c54
SystrN7 <felixgalaup@yahoo.fr>
2023-08-21 14:10:12 +0200

    fix print formatting remove scripts

--- P2/auto-deploy-group.sh
+++ P2/auto-deploy-group.sh
@@ -8,12 +8,10 @@ for container in `docker ps -q`; do
     echo  "Host: $container_hostname"
     echo  "Container: $container_name"
     echo  "Container ID: $container"
-    
-    printf "("$container_hostname")"
 
     # Router 1 (VTEP)
     if [ "$container_hostname" == "routeur_fgalaup-1" ]; then
-        echo " - routeur_fgalaup-1"
+        echo " - routeur_fgalaup-1 -"
         docker exec $container bash -c " \
         ip address add 20.1.1.1/24 dev eth0 && \
         ip link add name vxlan10 type vxlan id 10 group 239.1.1.1 dstport 4789 dev eth0 && \
@@ -26,7 +24,7 @@ for container in `docker ps -q`; do
 
     # Routeur 2 (VTEP)
     if [ "$container_hostname" == "routeur_fgalaup-2" ]; then
-        echo " - routeur_fgalaup-2"
+        echo " - routeur_fgalaup-2 -"
         docker exec $container bash -c " \
         ip address add 20.1.1.2/24 dev eth0 && \
         ip link add name vxlan10 type vxlan id 10 group 239.1.1.1 dstport 4789 dev eth0 && \
@@ -39,13 +37,13 @@ for container in `docker ps -q`; do
 
     # Host 1
     if [ "$container_hostname" == "host_fgalaup-1" ]; then
-        echo " - host_fgalaup-1"
+        echo " - host_fgalaup-1 -"
         docker exec $container bash -c "ip address add 30.1.1.1/24 dev eth0"
     fi
 
     # Host 2
     if [ "$container_hostname" == "host_fgalaup-2" ]; then
-        echo " - host_fgalaup-2"
+        echo " - host_fgalaup-2 -"
         docker exec $container bash -c "ip address add 30.1.1.2/24 dev eth0"
     fi
 done
