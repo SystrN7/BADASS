67b0febfaaf746417965b433b79d021936ddb9a4
SystrN7 <felixgalaup@yahoo.fr>
2023-08-21 13:43:24 +0200

    Add auto deploy scripts p2

---
+++ P2/auto-deploy-static.sh
@@ -0,0 +1,51 @@
+for container in `docker ps -q`; do 
+    # Get container info
+    container_name=$(docker inspect --format='{{.Name}}' $container)
+    container_hostname=$(docker exec -it $container hostname)
+    container_hostname=${container_hostname::-1}
+    
+    echo "----------------------------------------"
+    echo  "Host: $container_hostname"
+    echo  "Container: $container_name"
+    echo  "Container ID: $container"
+    
+    printf "("$container_hostname")"
+
+    # Router 1 (VTEP)
+    if [ "$container_hostname" == "routeur_fgalaup-1" ]; then
+        echo " - routeur_fgalaup-1"
+        docker exec $container bash -c " \
+        ip address add 20.1.1.1/24 dev eth0 && \
+        ip link add name vxlan10 type vxlan id 10 remote 20.1.1.2 dstport 4789 dev eth0 && \
+        ip link add name br0 type bridge && \
+        ip link set br0 up && \
+        ip link set vxlan10 up && \
+        ip link set vxlan10 master br0 && \
+        ip link set eth1 master br0"
+    fi
+
+    # Routeur 2 (VTEP)
+    if [ "$container_hostname" == "routeur_fgalaup-2" ]; then
+        echo " - routeur_fgalaup-2"
+        docker exec $container bash -c " \
+        ip address add 20.1.1.2/24 dev eth0 && \
+        ip link add name vxlan10 type vxlan id 10 remote 20.1.1.1 dstport 4789 dev eth0 && \
+        ip link add name br0 type bridge && \
+        ip link set br0 up && \
+        ip link set vxlan10 up && \
+        ip link set vxlan10 master br0 && \
+        ip link set eth1 master br0"
+    fi
+
+    # Host 1
+    if [ "$container_hostname" == "host_fgalaup-1" ]; then
+        echo " - host_fgalaup-1"
+        docker exec $container bash -c "ip address add 30.1.1.1/24 dev eth0"
+    fi
+
+    # Host 2
+    if [ "$container_hostname" == "host_fgalaup-2" ]; then
+        echo " - host_fgalaup-2"
+        docker exec $container bash -c "ip address add 30.1.1.2/24 dev eth0"
+    fi
+done
--- P2/script.sh
+++ P2/script.sh
@@ -1,33 +1,25 @@
+# Router 1 (VTEP)
+docker exec -it tender_solomon bash -c " \
+    ip address add 20.1.1.1/24 dev eth0 && \
+    ip link add name vxlan10 type vxlan id 10 remote 20.1.1.2 dstport 4789 dev eth0 && \
+    ip link add name br0 type bridge && \
+    ip link set br0 up && \
+    ip link set vxlan10 up && \
+    ip link set vxlan10 master br0 && \
+    ip link set eth1 master br0"
 
-# Router 1
-ip link add br0 type bridge # create the bridge with the name br0
-ip link set dev br0 up
-
-# Set ip ip addr add 10.1.1.1/24 dev eth0 (netwok betwen router)
-ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.2 local 10.1.1.1 dstport 4789
-ip addr add 20.1.1.1/24 dev vxlan10 # Assing ip to
-
-brctl addif br0 eth1
-brctl addif br0 vxlan10
-
-ip link set dev vxlan10 up
-
-
-# Router 2
-ip link add br0 type bridge
-ip link set dev br0 up
-
-# Set ip addr add  10.1.1.2/24 dev eth0
-
-ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.1 local 10.1.1.2 dstport 4789
-ip addr add 20.1.1.2/24 dev vxlan10
-ip link set dev vxlan10 up
-
-brctl addif br0 eth1
-brctl addif br0 vxlan10
+# Routeur 2 (VTEP)
+docker exec -it hungry_solomon bash -c " \
+    ip address add 20.1.1.2/24 dev eth0 && \
+    ip link add name vxlan10 type vxlan id 10 remote 20.1.1.1 dstport 4789 dev eth0 && \
+    ip link add name br0 type bridge && \
+    ip link set br0 up && \
+    ip link set vxlan10 up && \
+    ip link set vxlan10 master br0 && \
+    ip link set eth1 master br0"
 
 # Host 1
-ip addr add 30.1.1.1/24 dev eth0
+docker exec -it affectionate_proskuriakova bash -c "ip address add 30.1.1.1/24 dev eth0"
 
 # Host 2
-ip addr add 30.1.1.2/24 dev eth0
+docker exec -it inspiring_jennings bash -c "ip address add 30.1.1.2/24 dev eth0"
